121c84f2dd0386381c8b87230db93081c0c9ebf1
Ricardo  Camisa <70891837+ricardocamisa@users.noreply.github.com>
2023-10-02 05:44:51 +0100

    first-commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
